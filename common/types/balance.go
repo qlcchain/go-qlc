9bbc1bef94c7aa9889da59e9069779ab50deb644
pwc <659672152@qq.com>
2018-09-11 16:16:34 +0800

    add balance process func

---
+++ common/types/balance.go
@@ -0,0 +1,176 @@
+package types
+
+import (
+	"math/big"
+	"errors"
+	"github.com/shopspring/decimal"
+	"github.com/qlcchain/go-qlc/common/types"
+	"github.com/qlcchain/go-qlc/common"
+	"encoding/binary"
+)
+
+const (
+	// BalanceSize represents the size of a balance in bytes.
+	BalanceSize         = 16
+	BalanceMaxPrecision = 11
+)
+
+type BalanceComp byte
+
+const (
+	BalanceCompEqual   BalanceComp = iota
+	BalanceCompBigger
+	BalanceCompSmaller
+)
+
+var (
+	units = map[string]decimal.Decimal{
+		"raw":  decimal.New(1, 0),
+		"qlc":  decimal.New(1, 2),
+		"Kqlc": decimal.New(1, 5),
+		"Mqlc": decimal.New(1, 8),
+		"Gqlc": decimal.New(1, 11),
+	}
+
+	ZeroBalance = Balance(types.Uint128{})
+
+	ErrBadBalanceSize = errors.New("balances should be 16 bytes in size")
+)
+
+type Balance types.Uint128
+
+// ParseBalance parses the given balance string.
+func ParseBalance(s string, unit string) (Balance, error) {
+	d, err := decimal.NewFromString(s)
+	if err != nil {
+		return ZeroBalance, err
+	}
+
+	// zero is a special case
+	if d.Equals(decimal.Zero) {
+		return ZeroBalance, nil
+	}
+
+	d = d.Mul(units[unit])
+	c := d.Coefficient()
+	f := bigPow(10, int64(d.Exponent()))
+	i := c.Mul(c, f)
+
+	bytes := i.Bytes()
+	balanceBytes := make([]byte, BalanceSize)
+	copy(balanceBytes[len(balanceBytes)-len(bytes):], bytes)
+
+	var balance Balance
+	if err := balance.UnmarshalBinary(balanceBytes); err != nil {
+		return ZeroBalance, err
+	}
+
+	return balance, nil
+}
+
+func ParseBalanceInts(hi uint64, lo uint64) Balance {
+	return Balance(types.FromInts(hi, lo))
+}
+func ParseBalanceString(b string) Balance {
+	balance,err:=types.FromString(b)
+	if err!=nil{
+
+	}
+	return Balance(balance)
+}
+
+// Bytes returns the binary representation of this Balance with the given
+// endianness.
+func (b Balance) Bytes(order binary.ByteOrder) []byte {
+	bytes := types.Uint128(b).GetBytes()
+
+	switch order {
+	case binary.BigEndian:
+		return bytes
+	case binary.LittleEndian:
+		return common.ReverseBytes(bytes)
+	default:
+		panic("unsupported byte order")
+	}
+}
+
+// Equal reports whether this balance and the given balance are equal.
+func (b Balance) Equal(b2 Balance) bool {
+	return types.Uint128(b).Equal(types.Uint128(b2))
+}
+
+func (b Balance) Add(n Balance) Balance {
+	return Balance(types.Uint128(b).Add(types.Uint128(n)))
+}
+
+func (b Balance) Sub(n Balance) Balance {
+	return Balance(types.Uint128(b).Sub(types.Uint128(n)))
+}
+
+func (b Balance) Compare(n Balance) BalanceComp {
+	res := types.Uint128(b).Compare(types.Uint128(n))
+	switch res {
+	case 1:
+		return BalanceCompBigger
+	case -1:
+		return BalanceCompSmaller
+	case 0:
+		return BalanceCompEqual
+	default:
+		panic("unexpected comparison result")
+	}
+}
+
+// MarshalBinary implements the encoding.BinaryMarshaler interface.
+func (b Balance) MarshalBinary() ([]byte, error) {
+	return b.Bytes(binary.LittleEndian), nil
+}
+
+// UnmarshalBinary implements the encoding.BinaryUnmarshaler interface.
+func (b *Balance) UnmarshalBinary(data []byte) error {
+	if len(data) != BalanceSize {
+		return ErrBadBalanceSize
+	}
+
+	*b = Balance(types.FromBytes(data))
+	return nil
+}
+
+func (b Balance) BigInt() *big.Int {
+	i := big.NewInt(0)
+	i.SetBytes(b.Bytes(binary.BigEndian))
+	return i
+}
+
+// UnitString returns a decimal representation of this uint128 converted to the
+// given unit.
+func (b Balance) UnitString(unit string, precision int32) string {
+	d := decimal.NewFromBigInt(b.BigInt(), 0)
+	return d.DivRound(units[unit], BalanceMaxPrecision).Truncate(precision).String()
+}
+
+// String implements the fmt.Stringer interface. It returns the balance in Mxrb
+// with maximum precision.
+func (b Balance) String() string {
+	return b.UnitString("Mqlc", BalanceMaxPrecision)
+}
+
+func bigPow(base int64, exp int64) *big.Int {
+	return new(big.Int).Exp(big.NewInt(10), big.NewInt(exp), nil)
+}
+
+// MarshalText implements the encoding.TextMarshaler interface.
+func (b Balance) MarshalText() ([]byte, error) {
+	return []byte(b.String()), nil
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
+func (b *Balance) UnmarshalText(text []byte) error {
+	balance, err := ParseBalance(string(text), "Mqlc")
+	if err != nil {
+		return err
+	}
+
+	*b = balance
+	return nil
+}
---
+++ common/types/balance_test.go
@@ -0,0 +1,79 @@
+package types
+
+import (
+	"testing"
+)
+
+func TestQlcBalance(t *testing.T) {
+	b1, err := ParseBalance("600000000.00000000", "Mqlc")
+	b2, e := ParseBalance("1", "raw")
+	if err != nil {
+		t.Errorf("ParseBalance err for b1 %s",err)
+	}
+	if e != nil {
+		t.Errorf("ParseBalance err for b2 %s",e)
+	}
+	b1Units := map[string]string{
+		"raw":  "60000000000000000",
+		"qlc":  "600000000000000",
+		"Kqlc": "600000000000",
+		"Mqlc": "600000000",
+		"Gqlc": "600000",
+	}
+	b2Units := map[string]string{
+		"raw":  "1",
+		"qlc":  "0.01",
+		"Kqlc": "0.00001",
+		"Mqlc": "0.00000001",
+		"Gqlc": "0.00000000001",
+	}
+	b1TruncatedUnits := map[string]string{
+		"raw":  "60000000000000000",
+		"qlc":  "600000000000000",
+		"Kqlc": "600000000000",
+		"Mqlc": "600000000",
+		"Gqlc": "600000",
+	}
+
+	compare := func(b Balance, m map[string]string, p int32) {
+		for unit, s := range m {
+			res := b.UnitString(unit, p)
+			if res != s {
+				t.Errorf("(%s) expected: %s, got: %s\n", unit, s, res)
+			}
+		}
+	}
+
+	compare(b1, b1Units, BalanceMaxPrecision)
+	compare(b1, b1TruncatedUnits, 6)
+	compare(b2, b2Units, BalanceMaxPrecision)
+
+	if b1.String() != b1Units["Mqlc"] {
+		t.Errorf("unexpected fmt.Stringer result")
+	}
+
+	for unit, s := range b1Units {
+		b, err := ParseBalance(s, unit)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		if !b.Equal(b1) {
+			t.Errorf("(%s) expected: %s, got: %s\n", unit, b1.UnitString(unit, BalanceMaxPrecision), b.UnitString(unit, BalanceMaxPrecision))
+		}
+	}
+
+	for unit, s := range b1TruncatedUnits {
+		b, err := ParseBalance(s, unit)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		res := b.UnitString(unit, 6)
+		if res != s {
+			t.Errorf("(%s) expected: %s, got: %s\n", unit, s, res)
+		}
+	}
+}
