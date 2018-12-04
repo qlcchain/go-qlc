b1bbbe6b60a78ebbfd8900eb17dec12a4a97929d
Goren G <yong.gu@qlink.mobi>
2018-12-04 10:05:30 +0800

    add sign for hash

--- ci/travis.gofmt.sh
+++ ci/travis.gofmt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -n "$(goimports -l .| grep -v ^vendor/)" ]; then
+if [[ -n "$(goimports -l .| grep -v ^vendor/)" ]]; then
   echo "Go code is not formatted:"
   goimports -d .
   exit 1
