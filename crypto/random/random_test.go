8ca531df02c086656593a0f065cfe20a0abb0b03
Goren G <yong.gu@qlink.mobi>
2018-12-11 13:16:52 +0800

    add random test case

--- crypto/random/random_test.go
+++ crypto/random/random_test.go
@@ -6,3 +6,25 @@
  */
 
 package random
+
+import "testing"
+
+func TestBytes(t *testing.T) {
+	type args struct {
+		data []byte
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"test1", args{data: make([]byte, 32)}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := Bytes(tt.args.data); (err != nil) != tt.wantErr {
+				t.Errorf("Bytes() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
