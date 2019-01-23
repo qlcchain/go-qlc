b7165117b11756bb2433681a5e49ea91b1663f00
Goren G <yong.gu@qlink.mobi>
2019-01-23 14:14:51 +0800

    feat: add contract api - add smart contract api - remove useless code
    Signed-off-by: Goren G <yong.gu@qlink.mobi>

--- cmd/vm/main.go
+++ cmd/vm/main.go
@@ -4,58 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"github.com/qlcchain/go-qlc/vm/exec"
+	"github.com/qlcchain/go-qlc/vm/resolver"
 	"io/ioutil"
 	"strconv"
 	"time"
 )
-
-// Resolver defines imports for WebAssembly modules ran in Life.
-type Resolver struct {
-	tempRet0 int64
-}
-
-// ResolveFunc defines a set of import functions that may be called within a WebAssembly module.
-func (r *Resolver) ResolveFunc(module, field string) exec.FunctionImport {
-	fmt.Printf("Resolve func: %s %s\n", module, field)
-	switch module {
-	case "env":
-		switch field {
-		case "__life_ping":
-			return func(vm *exec.VirtualMachine) int64 {
-				return vm.GetCurrentFrame().Locals[0] + 1
-			}
-		case "__life_log":
-			return func(vm *exec.VirtualMachine) int64 {
-				ptr := int(uint32(vm.GetCurrentFrame().Locals[0]))
-				//msgLen := int(uint32(vm.GetCurrentFrame().Locals[1]))
-				msg, _ := vm.Memory.GetPointerMemory(uint64(ptr)) //[ptr : ptr+msgLen]
-				fmt.Printf("[app] %s\n", string(msg))
-				return 0
-			}
-
-		default:
-			panic(fmt.Errorf("unknown field: %s", field))
-		}
-	default:
-		panic(fmt.Errorf("unknown module: %s", module))
-	}
-}
-
-// ResolveGlobal defines a set of global variables for use within a WebAssembly module.
-func (r *Resolver) ResolveGlobal(module, field string) int64 {
-	fmt.Printf("Resolve global: %s %s\n", module, field)
-	switch module {
-	case "env":
-		switch field {
-		case "__life_magic":
-			return 424
-		default:
-			panic(fmt.Errorf("unknown field: %s", field))
-		}
-	default:
-		panic(fmt.Errorf("unknown module: %s", module))
-	}
-}
 
 func main() {
 	entryFunctionFlag := flag.String("entry", "app_main", "entry function id")
@@ -73,7 +26,7 @@ func main() {
 		EnableJIT:          *jitFlag,
 		DefaultMemoryPages: 128,
 		DefaultTableSize:   65536,
-	}, new(Resolver), nil)
+	}, new(resolver.Resolver), nil)
 
 	if err != nil {
 		panic(err)
---
+++ vm/contract/api.go
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018 QLC Chain Team
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package contract
+
+import "github.com/qlcchain/go-qlc/vm/exec"
+
+type ContractApi interface {
+	calloc(vm *exec.VirtualMachine) (bool, error)
+	malloc(vm *exec.VirtualMachine) (bool, error)
+	arrayLen(vm *exec.VirtualMachine) (bool, error)
+	memcpy(vm *exec.VirtualMachine) (bool, error)
+	memset(vm *exec.VirtualMachine) (bool, error)
+	//utility apis
+	stringcmp(vm *exec.VirtualMachine) (bool, error)
+	stringconcat(vm *exec.VirtualMachine) (bool, error)
+	Atoi(vm *exec.VirtualMachine) (bool, error)
+	Atoi64(vm *exec.VirtualMachine) (bool, error)
+	Itoa(vm *exec.VirtualMachine) (bool, error)
+	i64add(vm *exec.VirtualMachine) (bool, error)
+	QLC_ReadInt32Param(vm *exec.VirtualMachine) (bool, error)
+	QLC_ReadInt64Param(vm *exec.VirtualMachine) (bool, error)
+	QLC_ReadStringParam(vm *exec.VirtualMachine) (bool, error)
+	QLC_GetCallerAddress(vm *exec.VirtualMachine) (bool, error)
+	QLC_GetSelfAddress(vm *exec.VirtualMachine) (bool, error)
+	//storage
+	ONT_Storage_Put(vm *exec.VirtualMachine) (bool, error)
+	ONT_Storage_Get(vm *exec.VirtualMachine) (bool, error)
+	ONT_Storage_Delete(vm *exec.VirtualMachine) (bool, error)
+}
