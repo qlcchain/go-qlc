6c7a560009f1a7251acf48cc1bec7da22c8c10cc
Goren G <yong.gu@qlink.mobi>
2019-03-14 10:53:28 +0800

    chore: push both latest and tagged docker image
    Signed-off-by: Goren G <yong.gu@qlink.mobi>

--- docker/docker_push.sh
+++ docker/docker_push.sh
@@ -6,6 +6,7 @@ TAG="latest"
 if [[ $TRAVIS_TAG ]]; then
     TAG=$TRAVIS_TAG
     docker tag qlcchain/go-qlc:latest qlcchain/go-qlc:$TAG
+    docker push qlcchain/go-qlc:$TAG
 fi
 
-docker push qlcchain/go-qlc:$TAG
+docker push qlcchain/go-qlc:latest
