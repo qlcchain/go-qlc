0a8b8afad0960acc72de0f11423120ef76e64a9f
zengchen221 <931891076@qq.com>
2019-03-20 18:53:37 +0800

    feat: configure rpc module enable

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,15 +8,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/qlcchain/go-qlc"
 	"os"
-	"strconv"
 
 	cmd "github.com/qlcchain/go-qlc/cmd/server/commands"
 )
 
 func main() {
-	fmt.Println("main net: ", strconv.FormatBool(goqlc.MAINNET))
 	cmd.Execute(os.Args)
 }
