2db4cc8db56d816539c6b63dd76e19bd95305edc
Goren G <yong.gu@qlink.mobi>
2019-03-21 13:36:11 +0800

    refactor: move consensus test case to test folder
    Signed-off-by: Goren G <yong.gu@qlink.mobi>

--- test/consensus_test.go
+++ test/consensus_test.go
@@ -5,17 +5,11 @@
  * https://opensource.org/licenses/MIT
  */
 
-package main
+package test
 
 import (
 	"encoding/hex"
-	"errors"
 	"fmt"
-	"math/big"
-	"os"
-	"path/filepath"
-	"time"
-
 	"github.com/google/uuid"
 	"github.com/qlcchain/go-qlc/chain/services"
 	"github.com/qlcchain/go-qlc/common"
@@ -26,40 +20,21 @@ import (
 	"github.com/qlcchain/go-qlc/ledger/process"
 	"github.com/qlcchain/go-qlc/p2p"
 	"github.com/qlcchain/go-qlc/test/mock"
+	"math/big"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
 )
 
-var seed = "EE8B2F389880D23D1656652EDE146149A7A2E9DDCE2A95D511C55354CBB5ED50"
-var password = "123456"
-var address types.Address
-var ac *types.Account
-var token = common.ChainToken()
-var genesisBlock *types.StateBlock
+var (
+	seed         = "EE8B2F389880D23D1656652EDE146149A7A2E9DDCE2A95D511C55354CBB5ED50"
+	ac           *types.Account
+	token        = common.ChainToken()
+	genesisBlock *types.StateBlock
+)
 
-func creatGenesisBlock(l *ledger.Ledger) {
-	sByte, _ := hex.DecodeString(seed)
-	seed, _ := types.BytesToSeed(sByte)
-	ac, _ = seed.Account(0)
-	fmt.Println(ac.Address())
-	genesisBlock = createBlock(*ac, types.ZeroHash, token, types.Balance{Int: big.NewInt(int64(60000000000000000))}, types.Hash(ac.Address()), ac.Address())
-	verifier := process.NewLedgerVerifier(l)
-	verifier.BlockProcess(genesisBlock)
-}
-
-func importWallet(cfg *config.Config) error {
-	if len(seed) == 0 {
-		return errors.New("invalid seed")
-	}
-	w := services.NewWalletService(cfg)
-	addr, err := w.Wallet.NewWalletBySeed(seed, password)
-	if err != nil {
-		fmt.Println(err)
-	}
-	address = addr
-	fmt.Printf("import seed[%s]  => %s success", seed, addr.String())
-	return nil
-}
-
-func main() {
+func TestConsensus(t *testing.T) {
 	//bootNode config
 	dir := filepath.Join(config.QlcTestDataDir(), "consensus", uuid.New().String())
 	cfgFile, _ := config.DefaultConfig(dir)
@@ -98,14 +73,14 @@ func main() {
 	node1, err := p2p.NewQlcService(cfgFile1)
 	err = node1.Init()
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 	err = node1.Start()
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 	//import wallet
-	importWallet(cfgFile1)
+	//importWallet(cfgFile1)
 	//new dpos service
 	var accs []*types.Account
 	accs = append(accs, ac)
@@ -113,11 +88,11 @@ func main() {
 	//start node1 dpos service
 	err = consensusService1.Init()
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 	err = consensusService1.Start()
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 
 	//node2 config
@@ -137,61 +112,61 @@ func main() {
 	node2, err := p2p.NewQlcService(cfgFile2)
 	err = node2.Init()
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 	err = node2.Start()
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 
 	consensusService2 := services.NewDPosService(cfgFile2, node2, nil)
 	//start node2 dpos service
 	err = consensusService2.Init()
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 	err = consensusService2.Start()
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 
 	//remove test file
 	defer func() {
 		err := l.Ledger.Close()
 		if err != nil {
-			//t.Fatal(err)
+			t.Fatal(err)
 		}
 		err = ledger1.Ledger.Close()
 		if err != nil {
-			//t.Fatal(err)
+			t.Fatal(err)
 		}
 		err = consensusService1.Stop()
 		if err != nil {
-			//t.Fatal(err)
+			t.Fatal(err)
 		}
 		err = ledger2.Ledger.Close()
 		if err != nil {
-			//t.Fatal(err)
+			t.Fatal(err)
 		}
 		err = consensusService2.Stop()
 		if err != nil {
-			//t.Fatal(err)
+			t.Fatal(err)
 		}
 		err = node.Stop()
 		if err != nil {
-			//t.Fatal(err)
+			t.Fatal(err)
 		}
 		err = node1.Stop()
 		if err != nil {
-			//t.Fatal(err)
+			t.Fatal(err)
 		}
 		err = node2.Stop()
 		if err != nil {
-			//t.Fatal(err)
+			t.Fatal(err)
 		}
 		err = os.RemoveAll(config.QlcTestDataDir())
 		if err != nil {
-			//t.Fatal(err)
+			t.Fatal(err)
 		}
 	}()
 
@@ -221,7 +196,7 @@ func main() {
 	}
 	send, err := ledger1.Ledger.GenerateSendBlock(&sb, types.Balance{Int: big.NewInt(int64(1000))}, ac.PrivateKey())
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 	verifier1 := process.NewLedgerVerifier(ledger1.Ledger)
 	/**/ verifier1.Process(send)
@@ -229,19 +204,29 @@ func main() {
 	time.Sleep(30 * time.Second)
 	c, err := ledger2.Ledger.CountStateBlocks()
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 	if c != 2 {
-		//t.Fatal("node2 block count not correct")
+		t.Fatal("node2 block count not correct")
 	}
 
 	p, err := ledger2.Ledger.GetPerformanceTime(send.GetHash())
 	if err != nil {
-		//t.Fatal(err)
+		t.Fatal(err)
 	}
 	if p.T0 == 0 || p.T1 == 0 || p.T2 == 0 || p.T3 == 0 {
-		fmt.Println("send block confirmed error")
+		t.Fatal("send block confirmed error")
 	}
+}
+
+func creatGenesisBlock(l *ledger.Ledger) {
+	sByte, _ := hex.DecodeString(seed)
+	seed, _ := types.BytesToSeed(sByte)
+	ac, _ = seed.Account(0)
+	fmt.Println(ac.Address())
+	genesisBlock = createBlock(*ac, types.ZeroHash, token, types.Balance{Int: big.NewInt(int64(60000000000000000))}, types.Hash(ac.Address()), ac.Address())
+	verifier := process.NewLedgerVerifier(l)
+	verifier.BlockProcess(genesisBlock)
 }
 
 func createBlock(ac types.Account, pre types.Hash, token types.Hash, balance types.Balance, link types.Hash, rep types.Address) *types.StateBlock {
