9406ee2d1d1f4d5358977587a185c939607340a6
Goren G <yong.gu@qlink.mobi>
2019-03-22 20:24:29 +0800

    chore: echo sha256 hash for build file
    Signed-off-by: Goren G <yong.gu@qlink.mobi>

---
+++ ci/travis.sha256.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+which sha256sum
+
+if [[ $? -eq 0 ]]; then
+    if [ -d "build" ]; then
+        files=$(find build -type f)
+        for f in ${files}; do
+            echo $(sha256sum $f)
+        done
+    fi
+else
+    echo 'can not find sha256sum.'
+fi
+
