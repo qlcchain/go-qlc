99b02083cd0f966cbcfa8bd21bd91c89f50ef84d
wenchao <659672152@qq.com>
2019-04-17 21:02:47 +0800

    fix: genesis token info bug
    Signed-off-by: wenchao <659672152@qq.com>

--- vm/contract/abi/abi_mintage.go
+++ vm/contract/abi/abi_mintage.go
@@ -10,6 +10,7 @@ package abi
 import (
 	"errors"
 	"fmt"
+	"github.com/qlcchain/go-qlc/common"
 	"math/big"
 	"strings"
 
@@ -27,12 +28,14 @@ const (
 	[
 		{"type":"function","name":"Mintage","inputs":[{"name":"tokenId","type":"tokenId"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"},{"name":"totalSupply","type":"uint256"},{"name":"decimals","type":"uint8"},{"name":"beneficial","type":"address"},{"name":"NEP5TxId","type":"string"}]},
 		{"type":"function","name":"Withdraw","inputs":[{"name":"tokenId","type":"tokenId"}]},
-		{"type":"variable","name":"token","inputs":[{"name":"tokenId","type":"tokenId"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"},{"name":"totalSupply","type":"uint256"},{"name":"decimals","type":"uint8"},{"name":"owner","type":"address"},{"name":"pledgeAmount","type":"uint256"},{"name":"withdrawTime","type":"int64"},{"name":"pledgeAddress","type":"address"},{"name":"NEP5TxId","type":"string"}]}
+		{"type":"variable","name":"token","inputs":[{"name":"tokenId","type":"tokenId"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"},{"name":"totalSupply","type":"uint256"},{"name":"decimals","type":"uint8"},{"name":"owner","type":"address"},{"name":"pledgeAmount","type":"uint256"},{"name":"withdrawTime","type":"int64"},{"name":"pledgeAddress","type":"address"},{"name":"NEP5TxId","type":"string"}]},
+		{"type":"variable","name":"genesisToken","inputs":[{"name":"tokenId","type":"tokenId"},{"name":"tokenName","type":"string"},{"name":"tokenSymbol","type":"string"},{"name":"totalSupply","type":"uint256"},{"name":"decimals","type":"uint8"},{"name":"owner","type":"address"},{"name":"pledgeAmount","type":"uint256"},{"name":"withdrawTime","type":"int64"},{"name":"pledgeAddress","type":"address"}]}
 	]`
 
 	MethodNameMintage         = "Mintage"
 	MethodNameMintageWithdraw = "Withdraw"
 	VariableNameToken         = "token"
+	VariableNameGenesisToken  = "genesisToken"
 )
 
 var (
@@ -61,6 +64,31 @@ func ParseTokenInfo(data []byte) (*types.TokenInfo, error) {
 	}
 }
 
+func ParseGenesisTokenInfo(data []byte) (*types.TokenInfo, error) {
+	if len(data) == 0 {
+		return nil, errors.New("token info data is nil")
+	}
+	genesisTokenInfo := new(types.TokenInfo)
+
+	if err := MintageABI.UnpackVariable(genesisTokenInfo, VariableNameGenesisToken, data); err == nil {
+		tokenInfo := &types.TokenInfo{
+			TokenId:       genesisTokenInfo.TokenId,
+			TokenName:     genesisTokenInfo.TokenName,
+			TokenSymbol:   genesisTokenInfo.TokenSymbol,
+			TotalSupply:   genesisTokenInfo.TotalSupply,
+			Decimals:      genesisTokenInfo.Decimals,
+			Owner:         genesisTokenInfo.Owner,
+			PledgeAmount:  genesisTokenInfo.PledgeAmount,
+			WithdrawTime:  genesisTokenInfo.WithdrawTime,
+			PledgeAddress: genesisTokenInfo.PledgeAddress,
+			NEP5TxId:      "",
+		}
+		return tokenInfo, nil
+	} else {
+		return nil, err
+	}
+}
+
 func NewTokenHash(address types.Address, previous types.Hash, tokenName string) types.Hash {
 	h, _ := types.HashBytes(address[:], previous[:], util.String2Bytes(tokenName))
 	return h
@@ -74,10 +102,19 @@ func ListTokens(ctx *vmstore.VMContext) ([]*types.TokenInfo, error) {
 	var infos []*types.TokenInfo
 	if err := ctx.Iterator(types.MintageAddress[:], func(key []byte, value []byte) error {
 		if len(value) > 0 {
-			if info, err := ParseTokenInfo(value); err == nil {
-				infos = append(infos, info)
+			tokenId, _ := types.BytesToHash(key[33:])
+			if common.IsGenesisToken(tokenId) {
+				if info, err := ParseGenesisTokenInfo(value); err == nil {
+					infos = append(infos, info)
+				} else {
+					logger.Error(err)
+				}
 			} else {
-				logger.Error(err)
+				if info, err := ParseTokenInfo(value); err == nil {
+					infos = append(infos, info)
+				} else {
+					logger.Error(err)
+				}
 			}
 		}
 		return nil
