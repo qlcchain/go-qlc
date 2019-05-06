91124081c7ab6c07c05fd623632f8cd43a916830
wenyuangui <wenyuangui@yeah.net>
2019-05-06 16:20:14 +0800

    add pov getAccountState rpc

--- rpc/api/pov.go
+++ rpc/api/pov.go
@@ -1,9 +1,11 @@
 package api
 
 import (
+	"errors"
 	"github.com/qlcchain/go-qlc/common/types"
 	"github.com/qlcchain/go-qlc/ledger"
 	"github.com/qlcchain/go-qlc/log"
+	"github.com/qlcchain/go-qlc/trie"
 	"go.uber.org/zap"
 )
 
@@ -14,6 +16,10 @@ type PovApi struct {
 
 type PovApiBlock struct {
 	*types.PovBlock
+}
+
+type PovApiState struct {
+	*types.PovAccountState
 }
 
 func NewPovApi(ledger *ledger.Ledger) *PovApi {
@@ -57,4 +63,26 @@ func (api *PovApi) GetLatestBlock() (*PovApiBlock, error) {
 	}
 
 	return apiBlock, err
+}
+
+func (api *PovApi) GetAccountState(stateHash types.Hash, address types.Address) (*PovApiState, error) {
+	db := api.ledger.Store
+	stateTrie := trie.NewTrie(db, &stateHash, nil)
+
+	asBytes := stateTrie.GetValue(address.Bytes())
+	if len(asBytes) <= 0 {
+		return nil, errors.New("account value not exist")
+	}
+
+	as := new(types.PovAccountState)
+	err := as.Deserialize(asBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	apiState := &PovApiState{
+		PovAccountState: as,
+	}
+
+	return apiState, err
 }
