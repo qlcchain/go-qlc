821eacaf54d5dc34182fa6471a2021454d87ba23
wenchao <659672152@qq.com>
2019-05-15 13:11:35 +0800

    feat: generate test ledger support designated account
    Signed-off-by: wenchao <659672152@qq.com>

--- cmd/client/commands/generate_test_ledger.go
+++ cmd/client/commands/generate_test_ledger.go
@@ -21,13 +21,10 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	testGenesisAddress = "qlc_3hw8s1zubhxsykfsq5x7kh6eyibas9j3ga86ixd7pnqwes1cmt9mqqrngap4"
-)
-
 func generateTestLedger() {
 	var fromAccountP string
 	var repCountsP int
+	var toAccountsP []string
 	if interactive {
 		from := util.Flag{
 			Name:  "from",
@@ -41,11 +38,17 @@ func generateTestLedger() {
 			Usage: "rep counts",
 			Value: "",
 		}
+		to := util.Flag{
+			Name:  "to",
+			Must:  true,
+			Usage: "receive accounts",
+			Value: []string{},
+		}
 		c := &ishell.Cmd{
 			Name: "generateTestLedger",
 			Help: "generate test ledger",
 			Func: func(c *ishell.Context) {
-				args := []util.Flag{from, repCounts}
+				args := []util.Flag{from, repCounts, to}
 				if util.HelpText(c, args) {
 					return
 				}
@@ -55,7 +58,14 @@ func generateTestLedger() {
 				}
 				fromAccountP = util.StringVar(c.Args, from)
 				repCountsP, _ = util.IntVar(c.Args, repCounts)
-				err := generateLedger(fromAccountP, repCountsP)
+				toAccountsP = util.StringSliceVar(c.Args, to)
+				if len(toAccountsP) != 0 {
+					if (repCountsP - 1) != len(toAccountsP) {
+						util.Info("account count is not match rep count,pls check")
+						return
+					}
+				}
+				err := generateLedger(fromAccountP, repCountsP, toAccountsP)
 				if err != nil {
 					util.Info(err)
 					return
@@ -70,7 +80,7 @@ func generateTestLedger() {
 			Use:   "generateTestLedger",
 			Short: "generate test ledger",
 			Run: func(cmd *cobra.Command, args []string) {
-				err := generateLedger(fromAccountP, repCountsP)
+				err := generateLedger(fromAccountP, repCountsP, toAccountsP)
 				if err != nil {
 					cmd.Println(err)
 					return
@@ -80,31 +90,33 @@ func generateTestLedger() {
 		}
 		generateTestLedgerCmd.Flags().StringVar(&fromAccountP, "from", "", "send account private key")
 		generateTestLedgerCmd.Flags().IntVar(&repCountsP, "repCounts", 1, "rep counts")
+		generateTestLedgerCmd.Flags().StringSliceVar(&toAccountsP, "to", []string{}, "receive accounts")
 		rootCmd.AddCommand(generateTestLedgerCmd)
 	}
 }
 
-func generateLedger(fromAccountP string, repCountsP int) error {
+func generateLedger(fromAccountP string, repCountsP int, toAccountsP []string) error {
 	bytes, err := hex.DecodeString(fromAccountP)
 	if err != nil {
 		return err
 	}
 	fromAccount := types.NewAccount(bytes)
-	err = sendReceiveAndChangeAction(repCountsP, fromAccount)
+	err = sendReceiveAndChangeAction(repCountsP, fromAccount, toAccountsP)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func sendReceiveAndChangeAction(repCountsP int, from *types.Account) error {
+func sendReceiveAndChangeAction(repCountsP int, from *types.Account, toAccountsP []string) error {
 	client, err := rpc.Dial(endpointP)
 	if err != nil {
 		return err
 	}
 	defer client.Close()
 	var accountInfo *api.APIAccount
-	err = client.Call(&accountInfo, "ledger_accountInfo", testGenesisAddress)
+	addr := from.Address()
+	err = client.Call(&accountInfo, "ledger_accountInfo", addr)
 	if err != nil {
 		return err
 	}
@@ -117,57 +129,71 @@ func sendReceiveAndChangeAction(repCountsP int, from *types.Account) error {
 		}
 		fmt.Printf("totalBalance is [%s],amount is [%s]\n", totalBalance, amount)
 		for i := 0; i < (repCountsP - 1); i++ {
-			seed, err := types.NewSeed()
-			if err == nil {
-				if to, err := seed.Account(0); err == nil {
-					fmt.Println("Seed:", seed.String())
-					fmt.Println("Address:", to.Address())
-					fmt.Println("Private:", hex.EncodeToString(to.PrivateKey()))
-					para := api.APISendBlockPara{
-						From:      from.Address(),
-						TokenName: "QLC",
-						To:        to.Address(),
-						Amount:    amount,
-					}
-					var sendBlock types.StateBlock
-					err = client.Call(&sendBlock, "ledger_generateSendBlock", para, hex.EncodeToString(from.PrivateKey()))
-					if err != nil {
+			var para api.APISendBlockPara
+			var to *types.Account
+			if len(toAccountsP) == 0 {
+				seed, err := types.NewSeed()
+				if err == nil {
+					to, err = seed.Account(0)
+					if err == nil {
+						fmt.Println("Seed:", seed.String())
+						fmt.Println("Address:", to.Address())
+						fmt.Println("Private:", hex.EncodeToString(to.PrivateKey()))
+						para = api.APISendBlockPara{
+							From:      from.Address(),
+							TokenName: "QLC",
+							To:        to.Address(),
+							Amount:    amount,
+						}
+					} else {
 						return err
 					}
-					var h types.Hash
-					err = client.Call(&h, "ledger_process", &sendBlock)
-					if err != nil {
-						return err
-					}
-					var receiveBlock types.StateBlock
-					err = client.Call(&receiveBlock, "ledger_generateReceiveBlock", &sendBlock, hex.EncodeToString(to.PrivateKey()))
-					if err != nil {
-						return err
-					}
-					//from := sendBlock.Address
-					//s := fmt.Sprintf("Receive QLC from %s to %s （hash: %s）", from.String(), to.String(), receiveBlock.GetHash())
-					//if interactive {
-					//	Info(s)
-					//} else {
-					//	fmt.Println(s)
-					//}
-
-					var h1 types.Hash
-					err = client.Call(&h1, "ledger_process", &receiveBlock)
-					if err != nil {
-						return err
-					}
-					var changeBlock types.StateBlock
-					err = client.Call(&changeBlock, "ledger_generateChangeBlock", to.Address(), to.Address(), hex.EncodeToString(to.PrivateKey()))
-					if err != nil {
-						return err
-					}
-					var h2 types.Hash
-					err = client.Call(&h2, "ledger_process", &changeBlock)
-					if err != nil {
-						return err
-					}
+				} else {
+					return err
 				}
+			} else {
+				bytes, err := hex.DecodeString(toAccountsP[i])
+				if err != nil {
+					return err
+				}
+				to = types.NewAccount(bytes)
+				para = api.APISendBlockPara{
+					From:      from.Address(),
+					TokenName: "QLC",
+					To:        to.Address(),
+					Amount:    amount,
+				}
+			}
+			var sendBlock types.StateBlock
+			err = client.Call(&sendBlock, "ledger_generateSendBlock", para, hex.EncodeToString(from.PrivateKey()))
+			if err != nil {
+				return err
+			}
+			var h types.Hash
+			err = client.Call(&h, "ledger_process", &sendBlock)
+			if err != nil {
+				return err
+			}
+			var receiveBlock types.StateBlock
+			err = client.Call(&receiveBlock, "ledger_generateReceiveBlock", &sendBlock, hex.EncodeToString(to.PrivateKey()))
+			if err != nil {
+				return err
+			}
+
+			var h1 types.Hash
+			err = client.Call(&h1, "ledger_process", &receiveBlock)
+			if err != nil {
+				return err
+			}
+			var changeBlock types.StateBlock
+			err = client.Call(&changeBlock, "ledger_generateChangeBlock", to.Address(), to.Address(), hex.EncodeToString(to.PrivateKey()))
+			if err != nil {
+				return err
+			}
+			var h2 types.Hash
+			err = client.Call(&h2, "ledger_process", &changeBlock)
+			if err != nil {
+				return err
 			}
 		}
 	}
