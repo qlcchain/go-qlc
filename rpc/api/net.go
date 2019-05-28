d1b5103e4540d0b262794cf57e403e57f64b625d
wenchao <659672152@qq.com>
2019-05-28 14:11:19 +0800

    feat: add sync status rpc interface
    Signed-off-by: wenchao <659672152@qq.com>

--- rpc/api/net.go
+++ rpc/api/net.go
@@ -7,7 +7,12 @@ import (
 	"github.com/qlcchain/go-qlc/ledger"
 	"github.com/qlcchain/go-qlc/log"
 	"go.uber.org/zap"
+	"time"
 )
+
+const syncTimeout = 10 * time.Second
+
+var lastSyncTime int64
 
 type NetApi struct {
 	ledger *ledger.Ledger
@@ -15,7 +20,11 @@ type NetApi struct {
 	logger *zap.SugaredLogger
 }
 
+func syncingTime(t time.Time) {
+	lastSyncTime = t.Add(syncTimeout).UTC().Unix()
+}
 func NewNetApi(l *ledger.Ledger, eb event.EventBus) *NetApi {
+	_ = eb.Subscribe(string(common.EventSyncing), syncingTime)
 	return &NetApi{ledger: l, eb: eb, logger: log.NewLogger("api_net")}
 }
 
@@ -40,4 +49,11 @@ func (q *NetApi) ConnectPeersInfo() *PeersInfo {
 		Infos: p,
 	}
 	return i
+}
+func (q *NetApi) Syncing() bool {
+	now := time.Now().UTC().Unix()
+	if lastSyncTime < now {
+		return false
+	}
+	return true
 }
