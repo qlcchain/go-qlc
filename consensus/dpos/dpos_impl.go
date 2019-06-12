44e7310df7bef8d2ee9f34c1119049b790c29976
lichao <chao.li@qlink.mobi>
2019-06-12 14:11:19 +0800

    fix: mod proccess msg sleep interval

--- consensus/dpos/dpos_impl.go
+++ consensus/dpos/dpos_impl.go
@@ -248,7 +248,7 @@ func (dps *DPoS) ProcessMsgLoop() {
 			return
 		case bs := <-dps.blocks:
 			dps.ProcessMsgDo(bs)
-		case <-time.After(1 * time.Nanosecond):
+		case <-time.After(1 * time.Millisecond):
 			//
 		}
 	}
