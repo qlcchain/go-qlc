fd7614e03b8d0e886342aa29721fd245e22b7764
lichao <chao.li@qlink.mobi>
2019-07-09 18:12:26 +0800

    fix: optimize consensus

--- rpc/api/ledger.go
+++ rpc/api/ledger.go
@@ -372,6 +372,7 @@ func (l *LedgerApi) BlocksCount() (map[string]uint64, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	unCount, err := l.ledger.CountUncheckedBlocks()
 	if err != nil {
 		return nil, err
@@ -379,7 +380,7 @@ func (l *LedgerApi) BlocksCount() (map[string]uint64, error) {
 	c := make(map[string]uint64)
 	c["count"] = sbCount + scbCount
 	c["unchecked"] = unCount
-	//c["unchecked"] = consensus.GlobalUncheckedBlockNum.Load()
+
 	return c, nil
 }
 
@@ -392,6 +393,7 @@ func (l *LedgerApi) BlocksCount2() (map[string]uint64, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	unCount, err := l.ledger.CountUncheckedBlocks()
 	if err != nil {
 		return nil, err
@@ -399,7 +401,7 @@ func (l *LedgerApi) BlocksCount2() (map[string]uint64, error) {
 	c := make(map[string]uint64)
 	c["count"] = sbCount + scbCount
 	c["unchecked"] = unCount
-	//c["unchecked"] = consensus.GlobalUncheckedBlockNum.Load()
+
 	return c, nil
 }
 
