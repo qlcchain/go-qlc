9b93ae5ba6f37cfeab09d84d23efed193aec2fff
lichao <chao.li@qlink.mobi>
2019-07-10 15:57:20 +0800

    fix: optimize block check

--- consensus/dpos/active_transactions.go
+++ consensus/dpos/active_transactions.go
@@ -170,7 +170,7 @@ func (act *ActiveTrx) cleanVotes() {
 
 func (act *ActiveTrx) addWinner2Ledger(block *types.StateBlock) {
 	hash := block.GetHash()
-	act.dps.logger.Infof("block [%s] acked", hash)
+	act.dps.logger.Debugf("block [%s] acked", hash)
 
 	if exist, err := act.dps.ledger.HasStateBlock(hash); !exist && err == nil {
 		err := act.dps.lv.BlockProcess(block)
