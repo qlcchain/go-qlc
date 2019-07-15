9f197580daf6942f22780ddde0c0035659f80ac0
wenyuangui <wenyuangui@yeah.net>
2019-07-15 20:36:42 +0800

    fix: restore miner contract address to 3

--- common/types/address.go
+++ common/types/address.go
@@ -44,7 +44,7 @@ var (
 	MintageAddress, _    = HexToAddress("qlc_3qjky1ptg9qkzm8iertdzrnx9btjbaea33snh1w4g395xqqczye4kgcfyfs1")
 	NEP5PledgeAddress, _ = HexToAddress("qlc_3fwi6r1fzjwmiys819pw8jxrcmcottsj4iq56kkgcmzi3b87596jwskwqrr5")
 	RewardsAddress, _    = HexToAddress("qlc_3oinqggowa7f1rsjfmib476ggz6s4fp8578odjzerzztkrifqkqdz5zjztb3")
-	MinerAddress, _      = GenerateBuiltinContractAddress(11)
+	MinerAddress, _      = GenerateBuiltinContractAddress(3)
 
 	ChainContractAddressList = []Address{NEP5PledgeAddress, MintageAddress, RewardsAddress, MinerAddress}
 
