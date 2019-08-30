45fbc522dfb6d6aa3fd23e9420b35d6b0158bbdb
Goren G <yong.gu@qlink.mobi>
2019-08-30 15:23:10 +0800

    test: add test case for helper.go
    Signed-off-by: Goren G <yong.gu@qlink.mobi>

---
+++ chain/helper_test.go
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2019 QLC Chain Team
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+package chain
+
+import (
+	"github.com/qlcchain/go-qlc/chain/context"
+	"github.com/qlcchain/go-qlc/config"
+	"path/filepath"
+	"testing"
+)
+
+func TestRegisterServices(t *testing.T) {
+	cfgFile2 := filepath.Join(config.QlcTestDataDir(), "config1", config.QlcConfigFile)
+	cm := config.NewCfgManagerWithName(filepath.Dir(cfgFile2), filepath.Base(cfgFile2))
+	cc := context.NewChainContext(cm.ConfigFile)
+
+	if err := RegisterServices(cc); err != nil {
+		t.Fatal(err)
+	}
+
+	if services, err := cc.AllServices(); err != nil {
+		t.Fatal(err)
+	} else {
+		t.Log(len(services))
+	}
+}
