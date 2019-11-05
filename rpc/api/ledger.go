c0195ae6b80444d1ebc4de20eed85428a49e207b
wenchao <659672152@qq.com>
2019-11-05 13:27:03 +0800

    fix: openblock does not exit previous block
    Signed-off-by: wenchao <659672152@qq.com>

--- rpc/api/ledger.go
+++ rpc/api/ledger.go
@@ -868,11 +868,16 @@ func (l *LedgerApi) Process(block *types.StateBlock) (types.Hash, error) {
 
 		l.logger.Debug("broadcast block")
 		//TODO: refine
-		if b, _ := l.ledger.HasStateBlockConfirmed(block.Previous); !b {
-			l.logger.Debugf("wait for the previous block [%s] to confirm before broadcasting block [%s]", block.Previous.String(), hash.String())
-		} else {
+		if block.IsOpen() {
 			l.eb.Publish(common.EventBroadcast, p2p.PublishReq, block)
 			l.eb.Publish(common.EventGenerateBlock, flag, block)
+		} else {
+			if b, _ := l.ledger.HasStateBlockConfirmed(block.Previous); !b {
+				l.logger.Debugf("wait for the previous block [%s] to confirm before broadcasting block [%s]", block.Previous.String(), hash.String())
+			} else {
+				l.eb.Publish(common.EventBroadcast, p2p.PublishReq, block)
+				l.eb.Publish(common.EventGenerateBlock, flag, block)
+			}
 		}
 		return hash, nil
 	case process.BadWork:
