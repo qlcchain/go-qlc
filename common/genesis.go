623216f8b8b920a1798f60b38e09a30afd4191ee
wenchao <659672152@qq.com>
2019-11-25 14:57:29 +0800

    fix: genesisMintage block should also be genesis block
    Signed-off-by: wenchao <659672152@qq.com>

--- common/genesis.go
+++ common/genesis.go
@@ -89,7 +89,7 @@ func GasBlockHash() types.Hash {
 func IsGenesisBlock(block *types.StateBlock) bool {
 	hash := block.GetHash()
 	for _, v := range GenesisInfos {
-		if hash.String() == v.GenesisBlock.GetHash().String() {
+		if hash.Cmp(v.GenesisBlock.GetHash()) == 0 || hash.Cmp(v.GenesisMintageBlock.GetHash()) == 0 {
 			return true
 		}
 	}
@@ -99,7 +99,7 @@ func IsGenesisBlock(block *types.StateBlock) bool {
 // IsGenesis check token is chain token genesis
 func IsGenesisToken(hash types.Hash) bool {
 	for _, v := range GenesisInfos {
-		if hash.String() == v.GenesisMintageBlock.Token.String() {
+		if hash.Cmp(v.GenesisMintageBlock.Token) == 0 {
 			return true
 		}
 	}
