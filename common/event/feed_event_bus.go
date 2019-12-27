6a076a656b6a03e43037a19d80ffc6f0363fe7eb
wenyuangui <wenyuangui@yeah.net>
2019-12-27 10:59:30 +0800

    feat: add simple feed eventbus

---
+++ common/event/feed_event_bus.go
@@ -0,0 +1,83 @@
+package event
+
+import (
+	"sync"
+	"time"
+
+	ct "github.com/qlcchain/go-qlc/common/topic"
+)
+
+type FeedEventBus struct {
+	sync.RWMutex
+	feeds map[ct.TopicType]*Feed
+}
+
+func NewFeedEventBus() *FeedEventBus {
+	return &FeedEventBus{
+		feeds: make(map[ct.TopicType]*Feed),
+	}
+}
+
+func (eb *FeedEventBus) Subscribe(topic ct.TopicType, ch interface{}) FeedSubscription {
+	eb.Lock()
+	defer eb.Unlock()
+
+	f := eb.feeds[topic]
+	if f == nil {
+		f = &Feed{}
+	}
+	eb.feeds[topic] = f
+
+	sub := f.Subscribe(ch)
+	if sub == nil {
+		return nil
+	}
+
+	return sub
+}
+
+func (eb *FeedEventBus) Unsubscribe(sub FeedSubscription) {
+	sub.Unsubscribe()
+}
+
+func (eb *FeedEventBus) Publish(topic ct.TopicType, msg interface{}) {
+	f := eb.findFeed(topic)
+	if f == nil {
+		return
+	}
+
+	f.Send(msg)
+}
+
+func (eb *FeedEventBus) RpcSyncCall(msg *ct.EventRPCSyncCallMsg) interface{} {
+	if msg.ResponseChan == nil {
+		msg.ResponseChan = make(chan interface{})
+	}
+	eb.Publish(ct.EventRpcSyncCall, msg)
+	select {
+	case outRsp := <-msg.ResponseChan:
+		return outRsp
+	case <-time.After(time.Second):
+	}
+	return nil
+}
+
+func (eb *FeedEventBus) LookupFeed(topic ct.TopicType) *Feed {
+	eb.Lock()
+	f := eb.feeds[topic]
+	if f == nil {
+		f = &Feed{}
+	}
+	eb.feeds[topic] = f
+	eb.Unlock()
+
+	return f
+}
+
+func (eb *FeedEventBus) findFeed(topic ct.TopicType) *Feed {
+	eb.RLock()
+	f := eb.feeds[topic]
+	eb.RUnlock()
+
+	return f
+}
