462cdd6cc88ab596839a354aa539495c68700c61
Goren G <yong.gu@qlink.mobi>
2020-03-11 18:56:21 +0800

    test: fix serialize smart contract block test case
    Signed-off-by: Goren G <yong.gu@qlink.mobi>

--- common/types/block_contract_test.go
+++ common/types/block_contract_test.go
@@ -1,42 +1,39 @@
 package types
 
 import (
-	"encoding/json"
+	"math/rand"
 	"testing"
+
+	"github.com/qlcchain/go-qlc/crypto/random"
 )
 
-var contract = `
-  {
-    "internalAccount": "qlc_3oftfjxu9x9pcjh1je3xfpikd441w1wo313qjc6ie1es5aobwed5x4pjojic",
-    "contract": {
-      "abi": "mcvnzY+zF5mVDjsvknvPfFgRToMQAVI4wivQGRZBwerbUIvfrKD6/suZJWiFVOI5sbTa98bpY9+1cUhE2T9yidxSCpvZ4kkBVBMfcL3OJIqG",
-      "abiLength": 81,
-      "abiHash": "79dab43dcc97205918b297c3aba6259e3ab1ed7d0779dc78eec6f57e5d6307ce"
-    },
-    "owner": "qlc_1nawsw4yatupd47p3scd5x5i3s9szbsggxbxmfy56f8jroyu945i5seu1cdd",
-	"isUseStorage": false,
-	"extraAddress":[],
-    "address": "qlc_3watpnwym9i43kbkt35yfp8xnqo7c9ujp3b6udajza71mspjfzpnpdgoydzn",
-    "previous": "0000000000000000000000000000000000000000000000000000000000000000",
-    "extra": "0000000000000000000000000000000000000000000000000000000000000000",
-    "work": "00000000007bb1fe",
-    "signature": "0e5a3f6b246c99f20bbe302c4cdfb4639ef3358620a85dc996ba38b0e2ae463d3e4a1184b36abfa63d3d11f56c3242446d381f837f2fb6f7336cf303a5cbca08"
-  }
-`
-
 func TestSmartContractBlock_Serialize(t *testing.T) {
-	b := SmartContractBlock{}
-	err := json.Unmarshal([]byte(contract), &b)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(b)
+	var sb SmartContractBlock
+	account1 := account()
+	account2 := account()
 
-	if hash := b.GetHash(); hash.IsZero() {
+	masterAddress := account1.Address()
+	i := rand.Intn(100)
+	abi := make([]byte, i)
+	_ = random.Bytes(abi)
+	hash, _ := HashBytes(abi)
+	sb.Abi = ContractAbi{Abi: abi, AbiLength: uint64(i), AbiHash: hash}
+	sb.Address = account1.Address()
+	sb.Owner = account2.Address()
+
+	sb.InternalAccount = masterAddress
+	var w Work
+	h := sb.GetHash()
+	worker, _ := NewWorker(w, h)
+
+	sb.Work = worker.NewWork()
+	sb.Signature = account1.Sign(h)
+
+	if hash := sb.GetHash(); hash.IsZero() {
 		t.Fatal()
 	}
 
-	buff, err := b.Serialize()
+	buff, err := sb.Serialize()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -44,20 +41,21 @@ func TestSmartContractBlock_Serialize(t *testing.T) {
 	if err = b2.Deserialize(buff); err != nil {
 		t.Fatal(err)
 	}
-	t.Log(len(buff))
-	t.Log(b2.String())
 
-	//b2.ExtraAddress = []Address{b.Address, b.Address}
-	bytes, _ := json.Marshal(&b2)
-	t.Log(string(bytes))
-	if b2.IsUseStorage != b.IsUseStorage {
+	if b2.IsUseStorage != sb.IsUseStorage {
 		t.Fatal("isUseStorage error")
 	}
-	if b2.InternalAccount != b.InternalAccount {
+	if b2.InternalAccount != sb.InternalAccount {
 		t.Fatal("internalAccount error")
 	}
 
-	if s := b.IsValid(); s {
+	if s := sb.IsValid(); !s {
 		t.Fatal()
 	}
+}
+
+func account() *Account {
+	seed, _ := NewSeed()
+	_, priv, _ := KeypairFromSeed(seed.String(), 0)
+	return NewAccount(priv)
 }
