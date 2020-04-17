a8283872035728fd5a9eabaf1b654dc0609b99bb
wenyuangui <wenyuangui@yeah.net>
2020-04-17 14:07:35 +0800

    test: add privacy debug testcase

--- privacy/privacy_test.go
+++ privacy/privacy_test.go
@@ -131,3 +131,30 @@ func TestController_Receive(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestController_Debug(t *testing.T) {
+	tearDown, md := setupPrivacyTestCase(t, "http")
+	defer tearDown(t)
+
+	err := md.privacy.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	time.Sleep(10 * time.Millisecond)
+
+	feb := md.cc.FeedEventBus()
+	msgReq := &topic.EventRPCSyncCallMsg{Name: "Debug.PrivacyInfo"}
+	msgReq.In = make(map[string]interface{})
+	msgReq.Out = make(map[string]interface{})
+	msgRsp := feb.RpcSyncCall(msgReq)
+	time.Sleep(10 * time.Millisecond)
+
+	if msgRsp == nil {
+		t.Fatal("msgRsp is nil")
+	}
+
+	err = md.privacy.Stop()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
