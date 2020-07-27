cd616f04e5cd1dca807d7bfebe895516367d0d94
zengchen221 <931891076@qq.com>
2020-07-27 08:36:47 +0800

    feat: remove nep5 contract pendings

--- ledger/migration/migration.go
+++ ledger/migration/migration.go
@@ -276,6 +276,7 @@ func (m MigrationV15ToV16) Migrate(store storage.Store) error {
 		reset := false
 		bs := make([]bytesKV, 0)
 		pendingKvs := make([]*pendingKV, 0)
+		nep5PendingKvs := make([]*pendingKV, 0)
 
 		// get all pending infos from db
 		prefix, _ := storage.GetKeyOfParts(storage.KeyPrefixPending)
@@ -294,20 +295,22 @@ func (m MigrationV15ToV16) Migrate(store storage.Store) error {
 			if err := pi.Deserialize(value); err != nil {
 				return fmt.Errorf("pendingInfo deserialize err: %s", err)
 			}
-			// if pending is not from nep5 contract
-			if pi.Source != contractaddress.NEP5PledgeAddress {
-				pk := new(types.PendingKey)
-				if err := pk.Deserialize(key[1:]); err != nil {
-					if _, err := pk.UnmarshalMsg(key[1:]); err != nil {
-						return fmt.Errorf("pendingKey unmarshalMsg err: %s", err)
-					}
-					reset = true
+			pk := new(types.PendingKey)
+			if err := pk.Deserialize(key[1:]); err != nil {
+				if _, err := pk.UnmarshalMsg(key[1:]); err != nil {
+					return fmt.Errorf("pendingKey unmarshalMsg err: %s", err)
 				}
-				pkv := &pendingKV{
-					key:   pk,
-					value: value,
-				}
-				pendingKvs = append(pendingKvs, pkv)
+				reset = true
+			}
+			pkv := &pendingKV{
+				key:   pk,
+				value: value,
+			}
+			pendingKvs = append(pendingKvs, pkv)
+
+			// if pending is from nep5 contract
+			if pi.Source == contractaddress.NEP5PledgeAddress {
+				nep5PendingKvs = append(nep5PendingKvs, pkv)
 			}
 			return nil
 		})
@@ -353,10 +356,28 @@ func (m MigrationV15ToV16) Migrate(store storage.Store) error {
 						return err
 					}
 				}
+				for _, pkv := range nep5PendingKvs {
+					pKey, err := storage.GetKeyOfParts(storage.KeyPrefixPending, pkv.key)
+					if err != nil {
+						return err
+					}
+					if err := batch.Delete(pKey); err != nil {
+						return err
+					}
+				}
 				return updateVersion(m, batch)
 			})
 		} else {
 			return store.BatchWrite(false, func(batch storage.Batch) error {
+				for _, pkv := range nep5PendingKvs {
+					pKey, err := storage.GetKeyOfParts(storage.KeyPrefixPending, pkv.key)
+					if err != nil {
+						return err
+					}
+					if err := batch.Delete(pKey); err != nil {
+						return err
+					}
+				}
 				return updateVersion(m, batch)
 			})
 		}
