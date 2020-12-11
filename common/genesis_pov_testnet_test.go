469b7277c7801730cddc003ee52e3ef8c3c204ed
lcsmm <chao.li@qlink.mobi>
2020-12-11 14:10:42 +0800

    feat: lower pov difficulty (#1288)
    * feat: lower pov init difficulty
    
    * feat: lower pov initial difficulty for testnet
    
    Signed-off-by: lc3091 <chao.li@qlink.mobi>

--- common/genesis_pov_testnet_test.go
+++ common/genesis_pov_testnet_test.go
@@ -10,8 +10,7 @@ import (
 )
 
 func TestGenesisPovBlock1(t *testing.T) {
-	expectHash, _ := types.NewHash("091c23777ec5605488f71481ec56e228d05056e1451ce8ed33bd3b1a00000000")
-
+	expectHash, _ := types.NewHash("635232d442d0a9e54e38fb3a8f35697cac8360d5cece2d975e4834e646000000")
 	expectStateHash, _ := types.NewHash("1e78dcddbe569968e758251ada684d313104ca72285285e21cc381770fd3ee49")
 
 	checkStateHash := genesisPovBlock.GetStateHash()
@@ -26,10 +25,22 @@ func TestGenesisPovBlock1(t *testing.T) {
 		t.Fatal("invalid test net genesis pov block hash", checkHash.String(), expectHash.String())
 	}
 
-	powHash := genesisPovBlock.Header.ComputePowHash()
-	powInt := powHash.ToBigInt()
 	algoInt := genesisPovBlock.Header.GetAlgoTargetInt()
 	normInt := genesisPovBlock.Header.GetNormTargetInt()
+
+	// for nonce := uint32(0); nonce < uint32(0xFFFFFFFF); nonce += uint32(1) {
+	// 	genesisPovBlock.Header.BasHdr.Nonce = nonce
+	// 	chash := genesisPovBlock.Header.ComputePowHash()
+	// 	cint := chash.ToBigInt()
+	// 	if cint.Cmp(algoInt) < 0 {
+	// 		t.Log("get nonce: ", nonce)
+	// 		break
+	// 	}
+	// }
+
+	powHash := genesisPovBlock.Header.ComputePowHash()
+	powInt := powHash.ToBigInt()
+	t.Log(PovGenesisPowBits)
 	t.Logf("powInt %064x algoInt %064x", powInt, algoInt)
 	t.Logf("normInt %064x paramInt %064x", normInt, PovGenesisPowInt)
 	if powInt.Cmp(algoInt) > 0 {
