506886f29cf94c47745132b9f4ed563f2d6b01d6
zengchen221 <931891076@qq.com>
2021-02-24 20:56:00 +0800

    fix: dod settlement contracts change block fields

--- vm/contract/dod_settlement.go
+++ vm/contract/dod_settlement.go
@@ -585,6 +585,7 @@ func (uo *DoDSettleUpdateOrderInfo) DoReceive(ctx *vmstore.VMContext, block *typ
 	}
 
 	if block.Address.IsZero() {
+
 		// generate contract reward block
 		block.Type = types.ContractReward
 		block.Address = order.Seller.Address
